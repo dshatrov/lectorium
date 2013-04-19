9bdebb23a32ccdd9695ace2dc9eed44e73bc8b3e
Dmitry Shatrov <shatrov@gmail.com>
2013-04-19 09:12:32 +0200

    comments

--- lectorium/temporal_id_generator.h
+++ lectorium/temporal_id_generator.h
@@ -1,5 +1,23 @@
-#ifndef __LECTORIUM__TEMPORAL_ID_GENERATOR__H__
-#define __LECTORIUM__TEMPORAL_ID_GENERATOR__H__
+/*  Lectorium module for Moment Video Server
+    Copyright (C) 2011-2013 Dmitry Shatrov
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef LECTORIUM__TEMPORAL_ID_GENERATOR__H__
+#define LECTORIUM__TEMPORAL_ID_GENERATOR__H__
 
 
 #include <libmary/libmary.h>
@@ -97,5 +115,5 @@ public:
 }
 
 
-#endif /* __LECTORIUM__TEMPORAL_ID_GENERATOR__H__ */
+#endif /* LECTORIUM__TEMPORAL_ID_GENERATOR__H__ */
 
